de8f3b3e1e905234827f269f66c6f925ef97516b
Neel Indap <indap.n@husky.neu.edu>
2018-03-13 19:30:24 -0400

    initial commit

---
+++ resize.py
@@ -0,0 +1,18 @@
+import PIL
+from PIL import Image
+import os
+
+
+basewidth = 64
+hsize = 64
+dir = os.path.join(os.path.dirname(__file__),'kaggle_simpson_testset')
+new_path = os.path.join(os.path.dirname(__file__),'data','Simpsons')
+i=0
+
+for file in os.listdir(dir):
+    filename = os.fsdecode(os.path.join(dir,file))
+    img = Image.open(filename)
+    #wpercent = (basewidth / float(img.size[0]))
+    img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+    img.save(os.path.join(new_path,str(i)+".jpg"))
+    i += 1
