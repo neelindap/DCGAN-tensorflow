81717e491ac11d095e7fd82146297874e64d881b
Neel Indap <neelindap@gmail.com>
2018-04-21 11:37:28 -0400

    Blog update and images

--- model.py
+++ model.py
@@ -211,6 +211,7 @@ class DCGAN(object):
     # Start rotation at -90 degrees, end at 90 degrees and produce totally 14 images
     # sample_inputs = perspective_transform(sample_inputs)
 
+    # Add Gaussian Noise
     sample_inputs = add_gaussian_noise(sample_inputs)
     print(sample_inputs.shape)
 
